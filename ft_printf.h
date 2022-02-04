feb0916533cab83163ed7b9e2b3e2de946321c3d
Javier Cobos del Alamo <jcobos-d@student.42madrid.com>
2022-02-04 19:04:34 +0100

    header protecc

--- ft_printf.h
+++ ft_printf.h
@@ -6,8 +6,17 @@
 /*   By: jcobos-d <jcobos-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:38:29 by jcobos-d          #+#    #+#             */
-/*   Updated: 2022/02/04 18:46:42 by jcobos-d         ###   ########.fr       */
+/*   Updated: 2022/02/04 19:04:27 by jcobos-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
 int	ft_printf(const char *str, ...);
+
+#endif
