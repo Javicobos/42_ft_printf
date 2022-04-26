3a3b705efd00b42c14c57b47ad9993207098601e
Javier Cobos del Alamo <jcobos-d@student.42madrid.com>
2022-04-26 12:03:23 +0200

    normi edits

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,49 @@
 /*   By: jcobos-d <jcobos-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:38:30 by jcobos-d          #+#    #+#             */
-/*   Updated: 2022/03/21 17:38:00 by jcobos-d         ###   ########.fr       */
+/*   Updated: 2022/04/26 12:01:45 by jcobos-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_base(long nbr, char *base, int size);
+int	putnbr_b(long nbr, char *base, int size);
 int	ft_writeabs(int nbr, char *base);
 int	ft_putstr_fd_counter(char *s, int fd);
 int	ft_putptr(long nbr);
+int	print_classify(char c, va_list ap);
+
+int	print_classify(char c, va_list ap)
+{
+	int		retval;
+
+	retval = 0;
+	if (c == 's')
+		retval += ft_putstr_fd_counter(va_arg(ap, char *), 1);
+	else if (c == 'p')
+	{
+		retval += write(1, "0x", 2);
+		retval += putnbr_b(va_arg(ap, unsigned long), "0123456789abcdef", 16);
+	}
+		retval += ft_putptr((long)(va_arg(ap, void *)));
+	else if (c == 'd' || c == 'i')
+		retval += putnbr_b(va_arg(ap, int), "0123456789", 10);
+	else if (c == 'u')
+		retval += putnbr_b(va_arg(ap, unsigned int), "0123456789", 10);
+	else if (c == 'x')
+		retval += putnbr_b(va_arg(ap, unsigned int), "0123456789abcdef", 16);
+	else if (c == 'X')
+		retval += putnbr_b(va_arg(ap, unsigned int), "0123456789ABCDEF", 16);
+	else if (c == '%')
+		retval += write(1, "%", 1);
+	return (retval);
+}
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	arguments;
 	int		retval;
+	char	toprint;
 
 	va_start(arguments, str);
 	retval = 0;
@@ -30,26 +58,14 @@ int	ft_printf(const char *str, ...)
 			retval += write(1, str, 1);
 		else
 		{
-			if (*(str + 1) == 'c')
-			{
-				char c = va_arg(arguments, int);
-				retval += write(1, &c, 1); // could we do: retval += write(1, &va_arg(arguments, int), 1) ??
-			}
-			else if (*(str + 1) == 's')
-				retval += ft_putstr_fd_counter(va_arg(arguments, char *), 1);
-			else if (*(str + 1) == 'p')
-				retval += ft_putptr((long)(va_arg(arguments, void*)));
-			else if (*(str + 1) == 'd' || *(str + 1) == 'i')
-					retval += ft_putnbr_base(va_arg(arguments, int), "0123456789", 10);
-			else if (*(str + 1) == 'u')
-					retval += ft_putnbr_base(va_arg(arguments, unsigned int), "0123456789", 10);
-			else if (*(str + 1) == 'x')
-				retval += ft_putnbr_base(va_arg(arguments, unsigned int), "0123456789abcdef", 16);
-			else if (*(str + 1) == 'X')
-				retval += ft_putnbr_base(va_arg(arguments, unsigned int), "0123456789ABCDEF", 16);
-			else if (*(str + 1) == '%')
-				retval += write(1, "%", 1);
 			str++;
+			if (*(str) == 'c')
+			{
+				toprint = va_arg(arguments, int);
+				retval += write(1, &toprint, 1);
+			}
+			else
+				retval += print_classify(*(str), arguments);
 		}
 		str++;
 	}
@@ -66,13 +82,13 @@ int	ft_putstr_fd_counter(char *s, int fd)
 	pos = 0;
 	while (s[pos])
 	{
-		write(fd, s + pos, 1); // can I pos += write?
+		write(fd, s + pos, 1);
 		pos++;
 	}
 	return (pos);
 }
 
-int	ft_putnbr_base(long nbr, char *base, int size)
+int	putnbr_b(long nbr, char *base, int size)
 {
 	int	retval;
 
@@ -81,7 +97,7 @@ int	ft_putnbr_base(long nbr, char *base, int size)
 		retval += write(1, base, 1);
 	else if (nbr / size != 0)
 	{
-		retval += ft_putnbr_base(nbr / size, base, size);
+		retval += putnbr_b(nbr / size, base, size);
 		retval += ft_writeabs(nbr % size, base);
 	}
 	else
@@ -102,26 +118,4 @@ int	ft_writeabs(int nbr, char *base)
 	else
 		c = *(base + nbr);
 	return (write(1, &c, 1));
-}
-
-int	ft_putptr(long nbr)
-{
-	int		retval;
-	char	c;
-	long int		pow_hex;
-
-	//printf("my address is %l \n", nbr);
-	pow_hex = 4294967296;
-	retval = write(1, "0x", 2);
-	while (pow_hex > 0)
-	{
-		if (nbr / pow_hex < 10)
-			c = nbr / pow_hex + '0';
-		else
-			c = nbr / pow_hex + 'a' - 10;
-		retval += write(1, &c, 1);
-		nbr = nbr % pow_hex;
-		pow_hex = pow_hex / 16;
-	}
-	return (retval);
 }
